175e92c01094df5283902806c561ad2d68f916f6
Gage Heinrich <gheinrich@integrafinancialservice.com>
2024-07-29 11:12:07 -0600

    initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Gheinrich19\ChuckNorrisJokes;
+
+class JokeFactory
+{
+
+    public function __construct(
+        protected array       $premadeJokes = [
+            "Chuck Norris can divide by zero.",
+            "When Chuck Norris enters a room, he doesn't turn the lights on. He turns the dark off.",
+            "Chuck Norris can slam a revolving door.",
+            "Chuck Norris counted to infinity. Twice.",
+            "Chuck Norris can unscramble an egg.",
+            "When Chuck Norris does a push-up, he's pushing the Earth down.",
+            "Chuck Norris can hear sign language.",
+            "Chuck Norris can win a game of Connect Four in three moves.",
+            "Chuck Norris doesn't wear a watch. He decides what time it is.",
+            "Chuck Norris once made a Happy Meal cry."
+        ],
+        protected null|string $joke = null,
+        protected null|array  $jokes = null
+
+    )
+    {
+        if (is_null($this->jokes)) {
+            $this->jokes = $this->premadeJokes;
+        }
+
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
+    /**
+     * @return string
+     */
+    public function getJoke(): string
+    {
+        return $this->joke;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPremadeJokes(): array
+    {
+        return $this->premadeJokes;
+    }
+
+    public function setJoke(string $joke): void
+    {
+        $this->joke = $joke;
+
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gheinrich19\ChuckNorrisJokes\Tests;
+
+use Gheinrich19\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    #[Test]
+    public function it_returns_a_random_joke_test()
+    {
+        $jokeText = 'This is a joke';
+
+        $jokes = new JokeFactory(jokes: [$jokeText]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame($jokeText, $joke);
+    }
+
+    #[Test]
+    public function it_returns_a_predefined_joke()
+    {
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $jokes->getPremadeJokes());
+    }
+
+
+    #[Test]
+    public function it_sets_and_gets_a_specific_joke()
+    {
+        $jokeText = 'This is another joke';
+        $jokes = new JokeFactory();
+        $jokes->setJoke($jokeText);
+
+        $joke = $jokes->getJoke($jokeText);
+
+        $this->assertSame($jokeText, $joke);
+    }
+}
