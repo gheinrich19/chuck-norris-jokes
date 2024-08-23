5cef045efaabf750052a7e7ed6c54f886fd3b692
gheinrich <gheinrich@integrafinancialservice.com>
2024-08-23 10:21:48 -0600

    Add full path

--- src/Http/Controllers/ChuckNorrisController.php
+++ src/Http/Controllers/ChuckNorrisController.php
@@ -3,13 +3,12 @@
 namespace Gheinrich19\ChuckNorrisJokes\Http\Controllers;
 
 
-use Gheinrich19\ChuckNorrisJokes\Facades\ChuckNorris;
 use Illuminate\Routing\Controller;
 
 class ChuckNorrisController extends Controller
 {
     public function __invoke()
     {
-        return ChuckNorris::getRandomJoke();
+        return Gheinrich19\ChuckNorrisJokes\Facades\ChuckNorris::getRandomJoke();
     }
 }
